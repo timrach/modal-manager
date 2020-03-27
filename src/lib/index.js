8fb599d3314ecf51b4e7e10b90e23e6b2b3078e3
Tim Rach <tim.rach91@gmail.com>
2020-03-27 06:13:39 +0100

    renamed modal to modal-template

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,2 @@
-export { default as Modal } from './modal';
+export { default as Modal } from './modal-template';
 export { ModalContext, ACTION_SHOW_MODAL, ACTION_HIDE_MODAL, ModalContextProvider, useModalActions } from './modal-context';
